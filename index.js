c5899223b4f05436e1230fd5ba0d613b92059c0e
mozalymoza <mozalymoza@example.com>
2023-09-20 12:25:26 +0700

    INSERT DATA

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-// app.get('/',(req,res) => {
-//     res.send('halo dek')
-// })
+const bodyPs = require('body-parser');
+app.use(bodyPs.urlencoded({ extended: false}));
+app.use(bodyPs.json());
+
+
 const mhsRouter = require('./routes/mahasiswa');
 app.use('/api/mhs',mhsRouter);
 
--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const {body, validationResult } = require('express-validator');
+
 const connection = require('../config/db');
 
 router.get('/',function(req,res){
@@ -18,4 +20,37 @@ router.get('/',function(req,res){
         }
     })
 })
+
+router.post('/store', [
+    body('nama').notEmpty(),
+    body('nrp').notEmpty()
+], (req, res) => {
+    const error = validationResult (req);
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()
+        });
+    }
+    let Data = {
+        nama: req.body.nama,
+        nrp: req.body.nrp
+    }
+    connection.query('insert into mahasiswa set ?', Data, function(err, rows){
+        if (err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server Error',
+            })
+        }else{
+            return res.status(201).json({
+                status: true,
+                message: 'success..!',
+                data: rows[0]
+            })
+        }
+    })
+            
+        })
+
+        
 module.exports = router;
